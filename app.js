058ea712b57d3a7e35a49a4c1dbddb4ca1b67114
jerry <1503571487@163.com>
2026-06-21 11:34:31 +0800

    Initial commit: Express /hello endpoint

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/hello', (req, res) => {
+  res.json({ message: 'Hello AI Coding!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
